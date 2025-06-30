3bddffdb6f0bd853d176928b9ebca200085e14b8
Ada Wayran <ida.w.myran@gmail.com>
2025-06-30 13:51:49 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -12,5 +12,4 @@ const overlay = document.querySelector('.overlay');
 window.addEventListener('mousemove', e => {
   const x = e.clientX / window.innerWidth * 100;
   const y = e.clientY / window.innerHeight * 100;
-  overlay.style.background = `radial-gradient(circle at ${x}% ${y}%, rgba(0,0,63,0.2), rgba(0,0,0,0.9))`;
 });
